23a6d400ed26cc6dd2ec4654db428668c666f69f
Serkan Dağlıoğlu <serkandaglioglu@gmail.com>
2024-02-03 18:08:46 +0300

    first commit

---
+++ app/src/main/java/com/helikanonlibsample/testjitpack/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.helikanonlibsample.testjitpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.helikanonlibsample.helikanontestlib.HelikanonTestLibTutorial
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        HelikanonTestLibTutorial.test1("Deneme")
+    }
+}
---
+++ helikanontestlib/src/main/java/com/helikanonlibsample/helikanontestlib/HelikanonTestLibTutorial.kt
@@ -0,0 +1,9 @@
+package com.helikanonlibsample.helikanontestlib
+
+object HelikanonTestLibTutorial {
+
+    fun test1(s:String): String {
+        return "$s >> Serkan"
+    }
+
+}
